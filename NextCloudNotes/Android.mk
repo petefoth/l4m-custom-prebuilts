9857c5a64235656a6a0554d2dab44597ceee1f02
Pete Fotheringham <petefoth@e.email>
2022-08-17 22:49:48 +0100

    Add NextCloudNotes

---
+++ NextCloudNotes/Android.mk
@@ -0,0 +1,9 @@
+include $(CLEAR_VARS)
+LOCAL_MODULE := NextCloudNotes
+LOCAL_SRC_FILES := it.niedermann.owncloud.notes_3005000.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
