b06740fdc2c841f16da4773c572d376fa83dac94
Pete Fotheringham <petefoth@e.email>
2022-08-17 22:02:20 +0100

    Add Bliss Launcher

---
+++ BlissLauncher/Android.mk
@@ -0,0 +1,11 @@
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := BlissLauncher
+LOCAL_SRC_FILES := foundation.e.blisslauncher_13100.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+
+include $(BUILD_PREBUILT)
