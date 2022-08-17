68c5ee5e4ecd44d323ed4acacb2cc2e14a5f90e1
Pete Fotheringham <petefoth@e.email>
2022-08-17 22:50:52 +0100

    Add OpenCamera

---
+++ OpenCamera/Android.mk
@@ -0,0 +1,9 @@
+include $(CLEAR_VARS)
+LOCAL_MODULE := OpenCamera
+LOCAL_SRC_FILES := net.sourceforge.opencamera_85.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
