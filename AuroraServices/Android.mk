d442ed8daf553f660d2221f7a446817ed523c7a5
Pete Fotheringham <petefoth@e.email>
2022-08-18 15:15:10 +0100

    Add AuroraStore, BlissLauncher, Bromite, DAVx5, ICSx5, K9-Mail, NextCloud, NextCloudNotes, OpenCamera, OpenTasks, QKSMS, UpstreamEtar

--- AuroraServices/Android.mk
+++ AuroraServices/Android.mk
@@ -1,3 +1,19 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6cfebc5a04b7a81d1cb57e275349a4126b523d54f02765cf99f56feba3a5a177
-size 612
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := permissions_com.aurora.services.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := AuroraServices
+LOCAL_SRC_FILES := AuroraServices_v1.1.1.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_REQUIRED_MODULES := permissions_com.aurora.services.xml
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
